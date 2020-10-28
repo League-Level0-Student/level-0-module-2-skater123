2e2318479f5a5ae85ceb52aec640b16678ff5167
root <root@8a61f026446e>
2020-10-28 01:18:59 +0000

    Completed validation

--- src/_01_random/_4_validation/Validation.java
+++ src/_01_random/_4_validation/Validation.java
@@ -10,27 +10,111 @@ import javax.swing.JOptionPane;
 public class Validation {
 	public static void main(String[] args) {
 		
-		Random randomMaker = new Random();
-		
-		int randomNumber = randomMaker.nextInt(5);
-		
-		System.out.println(randomNumber);
-
-		// 1. Use each value of randomNumber to give the user a random compliment.
-		if (randomNumber == 0) {
-			JOptionPane.showMessageDialog(null,"I like your shoes");
-			
-		}
 		// 2. Repeat all the code above 10 times
-		 else if (randomNumber == 1){
-			 JOptionPane.showMessageDialog(null,"I like ya cut g") ;{
-		JOptionPane.showMessageDialog(null,"dang that t shirt is fresh");{
-			JOptionPane.showMessageDialog(null,"U  have such a nice smile");{
+		for(int i = 0; i<10; i++) {
+		
+			Random randomMaker = new Random();
+			
+			int randomNumber = randomMaker.nextInt(5);
+			
+			System.out.println(randomNumber);
+	
+			// 1. Use each value of randomNumber to give the user a random compliment.
+			if (randomNumber == 0) {
 				
-			}
-				 }
+				JOptionPane.showMessageDialog(null,"I like your shoes");
+				
+			} else if (randomNumber == 1) {
+				
+				//when randomNumber == 2
+				JOptionPane.showMessageDialog(null,"dang that t shirt is fresh");
+				
+				
+			 } else if(randomNumber == 2) {
+				 
+				 JOptionPane.showMessageDialog(null,"I like "
+							+ "your hairline friend") ;
+				 
+			 } else  if(randomNumber == 3) {
+				 
+				 JOptionPane.showMessageDialog(null,"u r my best  friend");
+				 
+			 } else  if(randomNumber == 4) {
+				 
+				 JOptionPane.showMessageDialog(null,"ur shoes r sooo cool");
 			 }
-		 }
+		}
+		
+		
+ 
 		// 3. Find someone to test out your program. They will like it :)
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
