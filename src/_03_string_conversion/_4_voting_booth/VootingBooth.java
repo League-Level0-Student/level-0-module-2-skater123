a4462a3d643d8569c0c8c1cfbb1d9223c2631f7d
root <root@09f8409a03cf>
2021-01-13 02:32:39 +0000

    vooting  booth

--- src/_03_string_conversion/_4_voting_booth/VootingBooth.java
+++ src/_03_string_conversion/_4_voting_booth/VootingBooth.java
@@ -3,36 +3,18 @@ package _03_string_conversion._4_voting_booth;
 import javax.swing.JOptionPane;
 
 public class VootingBooth {
-public static void main(String[] args) {
-String voting=	JOptionPane.showInputDialog(null,"HEY I GOT A QUESTION HOW OLD ARE YOU");	
-//int 
+	public static void main(String[] args) {
+		String voting = JOptionPane.showInputDialog(null, "HEY I GOT A QUESTION HOW OLD ARE YOU");
+		int votingtAsSint = Integer.parseInt(voting);
 
+		if (18 < votingtAsSint) {
+			JOptionPane.showMessageDialog(null, "alright man you get to vote");
+		}
 
+		if (18 > votingtAsSint) {
+			JOptionPane.showMessageDialog(null, "sorry little dude but you are too young");
 
+		}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
+	}
 }
